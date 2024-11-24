c8613ca013b3bcea9d67b6620b957b213c1601d6
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-11-24 22:08:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
 app.use('/code', code);
+
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
 })
@@ -20,10 +19,7 @@ res.sendFile(__path + '/main.html')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star
-
- Server running on http://localhost:` + PORT)
+    console.log(`⏩ Server running on http://localhost:` + PORT)
 })
 
 module.exports = app
